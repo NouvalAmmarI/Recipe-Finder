a252bdf8adbbce4d0aad8611f830f5bdd4d3d9e4
NouvalAmmarI <nouval.ibrahim45@gmail.com>
2024-06-18 15:27:38 +0700

    feat: rubah ke route baru

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,36 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Tasty Bites</title>
-  <link rel="stylesheet" href="css/home.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tasty Bites</title>
+    <link rel="stylesheet" href="css/home.css">
 </head>
 
 <body>
-  <div class="container">
-    <div class="glass-container">
-        <nav class="navbar">
-            <div class="logo"><a href="#">Tasty Bites</a></div>
-            <ul class="nav-links">
-                <li class="nav-link"><a href="FinalProject.html">Find Recipe</a></li>
-                <li class="nav-link"><a href="contact.html">Contact</a></li>
-            </ul>
-        </nav>
-        <div class="content">
-            <div class="main">
-                <h2>Hey! 👋,<br>It's Tasty Bites</h2>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. <br>
-                    Soluta ducimus dicta nisi quisquam rerum
-                </p>
-                <a href="/FinalProject"><button>Get Started</button></a>
-            </div>
-            <div class="image-wrapper">
-                <img src="img/chef.png">
+    <div class="container">
+        <div class="glass-container">
+            <nav class="navbar">
+                <div class="logo"><a href="#">Tasty Bites</a></div>
+                <ul class="nav-links">
+                    <li class="nav-link"><a href="/FinalProject">Find Recipe</a></li>
+                    <li class="nav-link"><a href="/contact">Contact</a></li>
+                </ul>
+            </nav>
+            <div class="content">
+                <div class="main">
+                    <h2>Hey! 👋,<br>It's Tasty Bites</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. <br>
+                        Soluta ducimus dicta nisi quisquam rerum
+                    </p>
+                    <a href="/FinalProject"><button>Get Started</button></a>
+                </div>
+                <div class="image-wrapper">
+                    <img src="img/chef.png">
+                </div>
             </div>
         </div>
     </div>
-</div>
 </body>
+
 </html>
