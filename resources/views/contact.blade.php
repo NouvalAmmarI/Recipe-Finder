d78e874d5d319ef05c9083f3c61eda598bd20010
NouvalAmmarI <nouval.ibrahim45@gmail.com>
2024-06-21 11:20:22 +0700

    feat:ganti form key

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,43 +1,45 @@
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Contact Us</title>
-  <link rel="stylesheet" href="css/contact.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Us</title>
+    <link rel="stylesheet" href="css/contact.css">
 </head>
 
 <body>
-  <div class="contact-container">
-    <form action="https://api.web3forms.com/submit" method="POST" class="contact-left">
+    <div class="contact-container">
+        <form action="https://api.web3forms.com/submit" method="POST" class="contact-left">
 
-      <div class="contact-left-title">
-        <h2>Contact Us</h2>
-        <hr>
-      </div>
+            <div class="contact-left-title">
+                <h2>Contact Us</h2>
+                <hr>
+            </div>
 
-      <input type="hidden" name="access_key" value="02086bf5-b999-4716-b1ef-dd76e1bb9df3">
+            <input type="hidden" name="access_key" value="f4b1e21d-a381-4ffa-8c2f-452fb6cc02de">
 
-      <input type="text" name="name" placeholder="Your Name" class="contact-inputs" required>
+            <input type="text" name="name" placeholder="Your Name" class="contact-inputs" required>
 
-      <input type="email" name="email" placeholder="Your Email" class="contact-inputs" required>
+            <input type="email" name="email" placeholder="Your Email" class="contact-inputs" required>
 
-      <textarea name="message" placeholder="Your Message" class="contact-inputs"></textarea>
+            <textarea name="message" placeholder="Your Message" class="contact-inputs"></textarea>
 
-      <div class="contact-button">
-        
-        <button type="submit">Submit <img src="img/arrow_icon.png" alt=""></button>
+            <div class="contact-button">
 
-        <button >
-          <a href="/FinalProject"> Back</a>
-        </button>
+                <button type="submit">Submit <img src="img/arrow_icon.png" alt=""></button>
 
-      </div>
-      
-    </form>
+                <button>
+                    <a href="/FinalProject"> Back</a>
+                </button>
 
-    <div class="contact-right">
-      <img src="img/chef1.png" alt="">
+            </div>
+
+        </form>
+
+        <div class="contact-right">
+            <img src="img/chef1.png" alt="">
+        </div>
     </div>
-  </div>
 </body>
+
 </html>
